1811c3d34cc6fa64d53d8c002e39f9e33cd2b091
maximmuss <roman.bv.js@gmail.com>
2016-09-30 18:05:26 +0300

    added new files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import User from '../components/User';
+import Page from '../components/Page';
+import * as pageActions from '../actions/PageActions'
 
 class App extends Component {
     render() {
-        const { name, surname, age } = this.props.user
+        const {user, page} = this.props;
+
         return <div>
-            <p>Привет из App, {name} {surname}!</p>
-            <p>Тебе уже {age} ?</p>
+            <User name={user.name}/>
+            <Page photos={page.photos} year={page.year}/>
         </div>
     }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
     return {
-        user: state
+        user: state.user,
+        page: state.page
     }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch) {
+    return {
+        pageActions: bindActionCreators(pageActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
